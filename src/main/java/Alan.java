fe2f3cc66ca993cb0d7094c855c7cfdfaf7f96aa
Alaneel <alanwang@Apples-MacBook-Pro.local>
2025-01-15 20:53:07 +0800

    Convert divider to constant

--- src/main/java/Alan.java
+++ src/main/java/Alan.java
@@ -1,10 +1,12 @@
 public class Alan {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        String divider = "____________________________________________________________";
+
+        System.out.println(divider);
         System.out.println("Hello! I'm Alan");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(divider);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(divider);
     }
 }
