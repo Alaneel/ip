e9ec5647c0d95233a56a02b1682decc29ba926c7
Alaneel <alanwang@Apples-MacBook-Pro.local>
2025-02-12 16:36:28 +0800

    Level-7

--- src/main/java/alan/core/Event.java
+++ src/main/java/alan/core/Event.java
@@ -10,6 +10,14 @@ public class Event extends Task {
         this.endTime = endTime;
     }
 
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + startTime + " to: " + endTime + ")";
